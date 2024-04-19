e29ef1b8b3f498b661dadeb86976b81ef16af30c
Harsh Sinha <hasinha@ggg.eecs.umich.edu>
2024-04-18 21:27:59 -0400

    get rid of build folder

--- freshAttempt/build/SwPrefetchPass/CMakeFiles/install-SwPrefetchPass.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/install-SwPrefetchPass"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/install-SwPrefetchPass.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
