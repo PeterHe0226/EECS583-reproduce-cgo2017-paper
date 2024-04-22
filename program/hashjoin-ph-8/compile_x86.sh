557bc6e913d19f100f50f1c15c0db6f589238722
Harsh Sinha <hasinha@ggg.eecs.umich.edu>
2024-04-21 22:25:15 -0400

    modify compile script to build test for compute c const prefetch pass

--- program/hashjoin-ph-8/compile_x86.sh
+++ program/hashjoin-ph-8/compile_x86.sh
@@ -4,5 +4,9 @@ clang -O3 npj2epb.c -Xclang -load -Xclang ../../../freshAttempt/build/swPrefetch
 clang -O3 npj2epb.ll -c 
 clang -O3 npj2epb.o main.c generator.c genzipf.c perf_counters.c cpu_mapping.c parallel_radix_join.c -lpthread -lm -std=c99  -o bin/x86/hj2-auto
 
+clang -O3 npj2epb.c -Xclang -load -Xclang ../../../freshAttempt/build/swPrefetchPass/SwPrefetchPass_new.so -c -S -emit-llvm 
+clang -O3 npj2epb.ll -c 
+clang -O3 npj2epb.o main.c generator.c genzipf.c perf_counters.c cpu_mapping.c parallel_radix_join.c -lpthread -lm -std=c99  -o bin/x86/hj2-auto-new
+
 clang -O3 npj2epb.c -c 
 clang -O3 npj2epb.o main.c generator.c genzipf.c perf_counters.c cpu_mapping.c parallel_radix_join.c -lpthread -lm -std=c99  -o bin/x86/hj2-no
