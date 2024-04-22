0c839c40b478ac10673bc71f2392cef68515b92b
Harsh Sinha <hasinha@ggg.eecs.umich.edu>
2024-04-21 22:25:15 -0400

    modify compile script to build test for compute c const prefetch pass

--- program/graph500/compile_x86.sh
+++ program/graph500/compile_x86.sh
@@ -2,5 +2,9 @@ clang -O3 seq-csr/seq-csr.c -Xclang -load -Xclang ../../freshAttempt/build/swPre
 clang -O3 seq-csr.ll -c 
 gcc -flto -g -std=c99 -Wall -O3 -I./generator   seq-csr.o graph500.c options.c rmat.c kronecker.c verify.c prng.c xalloc.c timer.c generator/splittable_mrg.c generator/graph_generator.c generator/make_graph.c generator/utils.c  -lm -lrt -o bin/x86/g500-auto
 
+clang -O3 seq-csr/seq-csr.c -Xclang -load -Xclang ../../freshAttempt/build/swPrefetchPass/SwPrefetchPass_new.so -c -S -emit-llvm 
+clang -O3 seq-csr.ll -c 
+gcc -flto -g -std=c99 -Wall -O3 -I./generator   seq-csr.o graph500.c options.c rmat.c kronecker.c verify.c prng.c xalloc.c timer.c generator/splittable_mrg.c generator/graph_generator.c generator/make_graph.c generator/utils.c  -lm -lrt -o bin/x86/g500-auto-new
+
 clang -O3 seq-csr/seq-csr.c -c 
 gcc -flto -g -std=c99 -Wall -O3 -I./generator   seq-csr.o graph500.c options.c rmat.c kronecker.c verify.c prng.c xalloc.c timer.c generator/splittable_mrg.c generator/graph_generator.c generator/make_graph.c generator/utils.c  -lm -lrt -o bin/x86/g500-no
