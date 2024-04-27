966045a27092db93b852e42ec51d1440ac7b44c2
Weiyuan Lyu <lyuweiyu@eecs583a.eecs.umich.edu>
2024-04-27 16:36:54 -0400

    add the test benchmark for mutiple time with mutiple constant value(for nas-cg right now)

---
+++ test_benchmark.py
@@ -0,0 +1,116 @@
+import re
+import sys
+import csv
+import argparse
+import subprocess
+import pathlib
+
+def parser_create():
+    parser = argparse.ArgumentParser(description='start c constant value and increment value')
+    parser.add_argument('--c', type=int, default=4, help='start constant c value to use')
+    parser.add_argument('--i', type=int, default=2, help='the power value of c value to increase each time')
+    parser.add_argument('--s', type=int, default=5, help='the time to increment the c value')
+    return parser
+
+
+def get_args():
+    global parser
+    parser = parser_create()
+    args = parser.parse_args()
+
+    return args
+
+def modify_cpp_constant(file_path, new_value):
+
+    pattern = re.compile(r'^#define C_CONSTANT \(\d+\)$')
+
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+
+    
+    with open(file_path, 'w') as file:
+        for line in lines:
+            if pattern.match(line.strip()):
+                # Replace the existing value with the new value
+                line = f'#define C_CONSTANT ({new_value})\n'
+            file.write(line)
+
+
+# modify_cpp_constant(pathlib.Path('./freshAttempt/swPrefetchPass/swPrefetchPass.cpp'), 88)
+
+def run_script_with_inputs(script_name, initial_input, exit_input, start_input):
+
+    process = subprocess.Popen(['python3', script_name], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+    stdout, stderr = process.communicate(input= start_input + '\n' + initial_input + '\n' + exit_input + '\n')
+    print("Output after initial input:", stdout)
+    if stderr:
+        print("Error:", stderr)
+
+    # # Check if process is still running, if not, no need to send 'e'
+    # if process.poll() is None:
+    #     process.stdin.write(exit_input + '\n')
+    #     process.stdin.flush()
+
+    # # process.stdin.close()
+
+    return_code = process.wait()
+    if return_code == 0:
+        print("Script executed successfully")
+    else:
+        print("Script exited with error")
+    
+    return stdout
+    
+# run_script_with_inputs('test_and_benchmark.py', '4', 'e', 'p')
+
+def catch_time_nas_cg(output):
+    time_pattern = re.compile(r"Time in seconds =\s+([\d\.]+)")
+    times = time_pattern.findall(output)
+
+    # print(times)
+    return times
+
+
+def write_to_csv(file_path, data):
+    with open(file_path, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        
+        writer.writerow(['no', 'auto', 'auto-new', 'Constant'])
+        
+        for row in data:
+            writer.writerow(row)
+
+def main():
+
+    args = get_args()
+
+    constant_val = args.c
+    increment_val = args.i
+    maximum_to_increment = args.s
+    
+    file_path = pathlib.Path('./freshAttempt/swPrefetchPass/swPrefetchPass.cpp')
+
+    exit_option = 'e'
+    build_option = 'p'
+    test_option = '4'
+    test_script_name = 'test_and_benchmark.py'
+
+    time_list = []
+
+    for _ in range(maximum_to_increment):
+        constant_val = constant_val * increment_val
+
+        modify_cpp_constant(file_path, constant_val)
+
+        output = run_script_with_inputs(test_script_name,test_option, exit_option,build_option)
+        output.append(constant_val)
+        if test_option == '4':
+            run_time = catch_time_nas_cg(output)
+            time_list.append(run_time)
+
+    
+    write_to_csv(f'test_benchmark_{test_option}.csv',time_list)
+
+
+main()
