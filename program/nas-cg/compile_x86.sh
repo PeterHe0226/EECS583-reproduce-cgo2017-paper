5f3e2fcd3d310f6bd9a78e4bda383fa5bae17007
PeterHe0226 <peterhe@umich.edu>
2024-04-27 11:33:55 -0400

    fixed nas-cg script

--- program/nas-cg/compile_x86.sh
+++ program/nas-cg/compile_x86.sh
@@ -23,6 +23,8 @@ parse_and_append_ipc() {
 
 }
 
+clang -O3 cg.c -c
+
 # Compile seq-csr to assembly
 clang -O3 -mllvm --x86-asm-syntax=intel -march=native -S cg.ll -o cg.s
 # Analyze the assembly with llvm-mca
@@ -32,7 +34,7 @@ parse_and_append_ipc "report.txt" "../../values.txt"
 clang -O3 -c cg.s -o cg.o
 clang -O3 cg.o ../nas-common/c_print_results.c ../nas-common/c_timers.c ../nas-common/wtime.c -lm ../nas-common/c_randdp.c -o bin/x86/cg-auto-new
 
-clang -O3 cg.c -c
+clang -O3 cg.ll -c
 clang -O3 cg.o ../nas-common/c_print_results.c ../nas-common/c_timers.c ../nas-common/wtime.c -lm ../nas-common/c_randdp.c -o bin/x86/cg-no
 clang -O3 cg.c -S -emit-llvm  -Xclang -load -Xclang ../../freshAttempt/build/swPrefetchPass/SwPrefetchPass.so
 clang -O3 cg.ll -c
