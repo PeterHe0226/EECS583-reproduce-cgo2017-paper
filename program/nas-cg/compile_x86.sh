8ca211fa0eb5232b92955cc0931aecadd57213a8
PeterHe0226 <peterhe@umich.edu>
2024-04-27 22:57:04 -0400

    fix

--- program/nas-cg/compile_x86.sh
+++ program/nas-cg/compile_x86.sh
@@ -34,6 +34,8 @@ parse_and_append_ipc "report.txt" "../../values.txt"
 clang -O3 -c cg.s -o cg.o
 clang -O3 cg.o ../nas-common/c_print_results.c ../nas-common/c_timers.c ../nas-common/wtime.c -lm ../nas-common/c_randdp.c -o bin/x86/cg-auto-new
 
+clang -O3 cg.c -c
+
 clang -O3 cg.ll -c
 clang -O3 cg.o ../nas-common/c_print_results.c ../nas-common/c_timers.c ../nas-common/wtime.c -lm ../nas-common/c_randdp.c -o bin/x86/cg-no
 clang -O3 cg.c -S -emit-llvm  -Xclang -load -Xclang ../../freshAttempt/build/swPrefetchPass/SwPrefetchPass.so
