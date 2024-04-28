da48997a5dabc8c65992e882766269f1b89be5b2
Harsh Sinha <hasinha@ggg.eecs.umich.edu>
2024-04-28 10:56:19 -0400

    clean hj8 folder

--- program/hashjoin-ph-8/compile_x86.sh
+++ program/hashjoin-ph-8/compile_x86.sh
@@ -1,5 +1,8 @@
 cd src
 
+[ ! -d "./bin" ] && mkdir ./bin
+[ ! -d "./bin/x86" ] && mkdir ./bin/x86
+
 clang -O3 npj2epb.c -Xclang -load -Xclang ../../../freshAttempt/build/swPrefetchPass/SwPrefetchPass.so -c -S -emit-llvm 
 clang -O3 npj2epb.ll -c 
 clang -O3 npj2epb.o main.c generator.c genzipf.c perf_counters.c cpu_mapping.c parallel_radix_join.c -lpthread -lm -std=c99  -o bin/x86/hj2-auto
