524deca702b3cf407f541765545a161978c97080
Harsh Sinha <hasinha@ggg.eecs.umich.edu>
2024-04-28 01:38:14 -0400

    specific ipc value for each test

--- program/nas-cg/compile_x86.sh
+++ program/nas-cg/compile_x86.sh
@@ -3,6 +3,6 @@ clang -O3 cg.o ../nas-common/c_print_results.c ../nas-common/c_timers.c ../nas-c
 clang -O3 cg.c -S -emit-llvm  -Xclang -load -Xclang ../../freshAttempt/build/swPrefetchPass/SwPrefetchPass.so
 clang -O3 cg.ll -c
 clang -O3 cg.o ../nas-common/c_print_results.c ../nas-common/c_timers.c ../nas-common/wtime.c -lm ../nas-common/c_randdp.c -o bin/x86/cg-auto
-clang -O3 cg.c -S -emit-llvm  -Xclang -load -Xclang ../../freshAttempt/build/swPrefetchPass/SwPrefetchPass_new.so
+clang -O3 cg.c -S -emit-llvm  -Xclang -load -Xclang ../../freshAttempt/build/swPrefetchPass/SwPrefetchPass_cg.so
 clang -O3 cg.ll -c
 clang -O3 cg.o ../nas-common/c_print_results.c ../nas-common/c_timers.c ../nas-common/wtime.c -lm ../nas-common/c_randdp.c -o bin/x86/cg-auto-new
--- program/randacc/compile_x86.sh
+++ program/randacc/compile_x86.sh
@@ -1,7 +1,7 @@
 clang -O3 randacc.c -Xclang -load -Xclang ../../freshAttempt/build/swPrefetchPass/SwPrefetchPass.so -c -S -emit-llvm
 clang -O3 randacc.ll -o bin/x86/randacc-auto
 
-clang -O3 randacc.c -Xclang -load -Xclang ../../freshAttempt/build/swPrefetchPass/SwPrefetchPass_new.so -c -S -emit-llvm
+clang -O3 randacc.c -Xclang -load -Xclang ../../freshAttempt/build/swPrefetchPass/SwPrefetchPass_rand.so -c -S -emit-llvm
 clang -O3 randacc.ll -o bin/x86/randacc-auto-new
 
 clang -O3 randacc.c -o bin/x86/randacc-no
