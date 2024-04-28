2c622c89c857d72cb6017b2ed09032dd707bdbf4
Harsh Sinha <hasinha@ggg.eecs.umich.edu>
2024-04-28 11:10:08 -0400

    cleanup nas-cg folder

--- program/nas-cg/compile_x86.sh
+++ program/nas-cg/compile_x86.sh
@@ -1,3 +1,6 @@
+[ ! -d "./bin" ] && mkdir ./bin
+[ ! -d "./bin/x86" ] && mkdir ./bin/x86
+
 clang -O3 cg.c -c
 clang -O3 cg.o ../nas-common/c_print_results.c ../nas-common/c_timers.c ../nas-common/wtime.c -lm ../nas-common/c_randdp.c -o bin/x86/cg-no
 clang -O3 cg.c -S -emit-llvm  -Xclang -load -Xclang ../../freshAttempt/build/swPrefetchPass/SwPrefetchPass.so
