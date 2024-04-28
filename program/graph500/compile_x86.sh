1bf8b06803d650b987559b41b2f9095ea5465fc9
Harsh Sinha <hasinha@ggg.eecs.umich.edu>
2024-04-28 10:44:10 -0400

    cleanup graph500 test

--- program/graph500/compile_x86.sh
+++ program/graph500/compile_x86.sh
@@ -1,3 +1,6 @@
+[ ! -d "./bin" ] && mkdir ./bin
+[ ! -d "./bin/x86" ] && mkdir ./bin/x86
+
 clang -O3 seq-csr/seq-csr.c -Xclang -load -Xclang ../../freshAttempt/build/swPrefetchPass/SwPrefetchPass.so -c -S -emit-llvm 
 clang -O3 seq-csr.ll -c 
 gcc -flto -g -std=c99 -Wall -O3 -I./generator   seq-csr.o graph500.c options.c rmat.c kronecker.c verify.c prng.c xalloc.c timer.c generator/splittable_mrg.c generator/graph_generator.c generator/make_graph.c generator/utils.c  -lm -lrt -o bin/x86/g500-auto
