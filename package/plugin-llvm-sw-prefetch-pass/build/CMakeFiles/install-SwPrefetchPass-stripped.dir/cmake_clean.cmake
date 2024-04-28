978d0d26211d732399e4ac51478398a00d4968da
Harsh Sinha <hasinha@ggg.eecs.umich.edu>
2024-04-27 23:33:34 -0400

    remove package

--- package/plugin-llvm-sw-prefetch-pass/build/CMakeFiles/install-SwPrefetchPass-stripped.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/install-SwPrefetchPass-stripped"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/install-SwPrefetchPass-stripped.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
